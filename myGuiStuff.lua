97fdac0dedb55ba11f4860bfc663d56c03e5a660
Jlectric <91878745+JKEYS2@users.noreply.github.com>
2023-09-23 17:40:18 +0100

    added Label class

--- myGuiStuff.lua
+++ myGuiStuff.lua
@@ -9,28 +9,30 @@ local colourGreen = 0x00FF00
 
 
 
-RectanglePoints = {}
-function RectanglePoints:new(posX, posY, width, height)
-    local t = setmetatable({}, { __index = RectanglePoints})
+-- RectanglePoints = {}
+-- function RectanglePoints:new(posX, posY, width, height)
+--     local t = setmetatable({}, { __index = RectanglePoints})
 
-    t.posX = (posX or 2)
-    t.posY = (posY or 2)
-    t.width = (width or 10)
-    t.height = (height or 10)
+--     t.posX = (posX or 2)
+--     t.posY = (posY or 2)
+--     t.width = (width or 10)
+--     t.height = (height or 10)
 
-    return t
-end
+--     return t
+-- end
 
 Box = {}
 function Box:new(name, posX, posY, width, height, colour, borderColour)
     local t = setmetatable({}, { __index = Box})
 
+    t.name = name
     t.posX = (posX or 2)
     t.posY = (posY or 2)
     t.width = (width or 10)
     t.height = (height or 10)
     t.colour = (colour or 0x00FF00) -- default green
     t.borderColour = (borderColour or 0xD2D2D2) -- default grey
+    t.labels = {}
 
     return t
 end
@@ -64,7 +66,41 @@ function Box:draw()
 end
 
 
+Label = {}
+function Label:new(name, text, textColour, posX, posY)
+    local t = setmetatable({}, { __index = Label})
 
-local box = Box:new("box", 30,2, 25,15, colourGreen, colourGrey)
+    t.name = name
+    t.text = (text or "I'm so empty")
+    t.textColour = (textColour or 0xFFFFFF)
+    t.backgroundColour = (backgroundColour or 0x000000)
+    t.posX = (posX or 1)
+    t.posY = (posY or 1)
+
+    return t
+end
+
+function Label:sayHi()
+    print("Hi I'm "..self.name)
+end
+
+
+-- Border = {}
+-- function Border:new(name, posX, posY, width, height, colour, borderColour)
+--     local t = setmetatable({}, { __index = Border})
+
+--     t.posX = (posX or 2)
+--     t.posY = (posY or 2)
+--     t.width = (width or 10)
+--     t.height = (height or 10)
+--     t.colour = (colour or 0x00FF00) -- default green
+--     t.borderColour = (borderColour or 0xD2D2D2) -- default grey
+
+--     return t
+-- end
+
+local box = Box:new("box", 30,2, 25,15, colourGreen, colourGrey, testThing)
+local lable:new("new", "Read Me!")
+-- table.insert(box.lables,)
 box:toString()
 box:draw()
