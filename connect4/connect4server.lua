9ff995f6960c7187b8bd0c77ec09531ddf7cc51a
Jlectric <91878745+JKEYS2@users.noreply.github.com>
2024-02-03 17:38:26 +0000

    Add uncommitted connect4

---
+++ connect4/connect4server.lua
@@ -0,0 +1,14 @@
+
+
+-- start game
+
+--* While no winner:
+-- check board for valid columns 
+-- get user input --> 7 columns or walk on the part of the screen you want to select?
+-- add move to board data
+-- send data to robot to place move
+
+-- send board to api
+-- end if there is a winner
+-- choose best move
+-- send data to robot to place move
